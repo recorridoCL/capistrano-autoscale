114aa88a88d34c12ed3e25e065208596fffd9802
Benjamín Silva <silva96@gmail.com>
2017-12-22 10:51:24 -0300

    add methods and helpers

--- lib/capistrano/autoscale.rb
+++ lib/capistrano/autoscale.rb
@@ -1,7 +1,21 @@
-#require 'capistrano/autoscale/configuration'
 load File.expand_path('../autoscale/tasks/autoscale.rake', __FILE__)
 require 'capistrano/all'
+require 'capistrano/autoscale/helpers/aws_utils'
 
 def setup_servers
-  puts "I'm setting up servers #{fetch(:instance_type)}"
+  puts "Search instances to deploy"
+  ec2_instances = AwsUtils.fetch_ec2_instances(fetch(:instance_type))
+  aws_deploy_user = fetch(:deploy_user)
+  set :instances, ec2_instances.map {|i| i[:instance_id]}
+  ec2_instances.each {|instance|
+    if ec2_instances.first == instance
+      roles = %w{web app db}
+      server instance[:private_ip_address], user: aws_deploy_user, roles:  roles, primary: true
+      puts "First Server: #{instance[:private_ip_address]} - #{roles}, instance_id: #{instance[:instance_id]}"
+    else
+      roles = %w{web app}
+      server instance[:private_ip_address], user: aws_deploy_user, roles: roles
+      puts "Server: #{instance[:private_ip_address]} - #{roles}, instance_id: #{instance[:instance_id]}"
+    end
+  }
 end
---
+++ lib/capistrano/autoscale/helpers/aws_utils.rb
@@ -0,0 +1,29 @@
+module Capistrano
+  module Autoscale
+    class AwsUtils
+      include Capistrano::DSL
+      def self.fetch_ec2_instances(type)
+        Aws.config[:region] = fetch(:aws_region)
+        Aws.config[:credentials] = Aws::Credentials.new(fetch(:aws_access_owner_id), fetch(:aws_secret_owner_access_key))
+
+        loadbalancer = Aws::ElasticLoadBalancingV2::Client.new
+        ec2 = Aws::EC2::Client.new
+
+        loadbalancer_data = loadbalancer.describe_target_health({
+                                                                    target_group_arn: fetch(:autoscaling_target_group_arn)
+                                                                })
+
+        instances_ids = loadbalancer_data.target_health_descriptions.map{|h| h.target.id}.sort
+
+        type_instances = instances_ids.values_at(* instances_ids.each_index.select {|i| i.send("#{type}?")})
+        description_instances = ec2.describe_instances({instance_ids: type_instances}).reservations
+
+        instances = description_instances.map{|h| h.instances.map {|i| {instance_id: i.instance_id, private_ip_address: i.private_ip_address}}}.flatten
+
+        puts "Found #{type} #{instances.count} servers (#{instances.join(',')})"
+
+        instances
+      end
+    end
+  end
+end
