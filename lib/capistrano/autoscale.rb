f05d0c203e980c5767c2356ad6bdf1dc3679607e
Benjamín Silva <silva96@gmail.com>
2017-12-22 10:31:05 -0300

    use puts outside of on() hooks

--- lib/capistrano/autoscale.rb
+++ lib/capistrano/autoscale.rb
@@ -1,6 +1,7 @@
 #require 'capistrano/autoscale/configuration'
 load File.expand_path('../autoscale/tasks/autoscale.rake', __FILE__)
+require 'capistrano/all'
 
 def setup_servers
-  info "I'm setting up servers #{fetch(:instance_type)}"
+  puts "I'm setting up servers #{fetch(:instance_type)}"
 end
